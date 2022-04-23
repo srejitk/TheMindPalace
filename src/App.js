4d0b1f7dcff7c04d4fc273016123bb5eb7b09421
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-04-23 23:52:02 +0530

    Feat : Added Token Component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 import logo from "./logo.png";
 import Router from "./utils/Router";
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Router />
+      <ToastContainer />
     </div>
   );
 }
---
+++ src/components/index.jsx
@@ -0,0 +1,3 @@
+import Toast from "./Toast/Toast";
+
+export { Toast };
