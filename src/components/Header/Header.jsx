bc6916f0387603a6a3a0b5f26e23a0f2217251c4
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-04-27 02:28:34 +0530

    Feat : Added Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../context/Auth/AuthContext";
+import { useTheme } from "../../context/Theme/ThemeContext";
+
+export default function Header() {
+  const { isLogged, logoutHandler } = useAuth();
+  const { sidebar, setSidebar, theme, setTheme } = useTheme();
+
+  const handleSidebar = (e) => {
+    setSidebar((prev) => !prev);
+  };
+
+  const handleTheme = (e) => {
+    setTheme(!theme);
+  };
+
+  return (
+    <div className={`flex-row-wrap position-relative ${styles.Navbar}`}>
+      <div className={`flex-row-wrap gap20 ${styles.Navbar_brand}`}>
+        <button onClick={(e) => handleSidebar(e)} className="btn btn_action ">
+          <span className="material-icons">{sidebar ? `close` : `menu`}</span>
+        </button>
+
+        <Link to="/" className={`flex-row-wrap gap20 ${styles.Navbar_brand}`}>
+          <div className={`flex-mid-center ${styles.logo_container}`}>
+            <img
+              className={styles.logo}
+              src="https://res.cloudinary.com/dkqrmlxlg/image/upload/v1649100763/Brain%20Dump/Brain_dlnboa.svg"
+              alt="BrainDump Logo"
+            />
+          </div>
+          <h5 className="header-5">The Mind Palace</h5>
+        </Link>
+      </div>
+
+      {isLogged ? (
+        <div className={`flex-row-wrap flex-mid-center gap20`}>
+          <Link
+            to="sign-up"
+            onClick={logoutHandler}
+            className="links  btn btn_action subtitle-1"
+          >
+            Logout
+          </Link>
+        </div>
+      ) : (
+        <div className={`flex-row-wrap flex-mid-center gap20`}>
+          <Link to="/login" className="links btn btn_action subtitle-1">
+            Sign In
+          </Link>
+          <Link to="sign-up" className="links btn btn_action subtitle-1">
+            Sign Up
+          </Link>
+        </div>
+      )}
+      <button onClick={(e) => handleTheme(e)} className="btn btn_action ">
+        <span className="material-icons">
+          {theme ? `light_mode` : `dark_mode`}
+        </span>
+      </button>
+    </div>
+  );
+}
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,4 @@
+import Header from "./Header/Header";
 import Toast from "./Toast/Toast";
 
-export { Toast };
+export { Toast, Header };
