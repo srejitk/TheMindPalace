bc80a9e5fc61dad8c341a1e100154e48eeee991a
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-04-29 00:22:28 +0530

    Feat : Added useAxios Custom Hook

---
+++ src/utils/useAxios.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+const useAxios = (pathname) => {
+  const [isLoading, setIsloading] = useState(false);
+  const [responseData, setResponseData] = useState();
+  const [errorFlag, setErrorFlag] = useState(false);
+  const apiCall = async () => {
+    setIsloading(true);
+    try {
+      const response = await axios.get(pathname);
+      if (response.status === 200) {
+        console.log(response);
+        setResponseData(response.data);
+      }
+    } catch (error) {
+      console.log("Something went wrong", error);
+      setErrorFlag(true);
+    } finally {
+      setIsloading(false);
+    }
+  };
+
+  useEffect(() => {
+    apiCall();
+  }, []);
+  return { isLoading, responseData, errorFlag };
+};
+
+export default useAxios;
