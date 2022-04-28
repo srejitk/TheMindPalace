cce69701db6077844dcaab5301e0f145081de90b
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-04-29 00:22:55 +0530

    Feat : Added VideoContext

---
+++ src/context/Video/VideoContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useReducer, useEffect } from "react";
+import useAxios from "../../utils/useAxios";
+import { VideoReducer } from "./VideoReducer";
+
+VideoReducer;
+
+const VideoContext = createContext();
+
+const useVideo = () => useContext(VideoContext);
+
+const VideoProvider = ({ children }) => {
+  const [videoState, videoDispatch] = useReducer(VideoReducer, {
+    videolist: [],
+  });
+  const { isLoading, responseData, errorFlag } = useAxios("/api/videos");
+
+  useEffect(() => {
+    videoDispatch({ type: "GET_VIDEOS", payload: responseData?.videos });
+  }, [isLoading]);
+
+  return (
+    <VideoContext.Provider
+      value={{ isLoading, videoState, videoDispatch, errorFlag }}
+    >
+      {children}
+    </VideoContext.Provider>
+  );
+};
+
+export { useVideo, VideoProvider };
