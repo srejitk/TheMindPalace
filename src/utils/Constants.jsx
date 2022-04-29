0533991e84f7abed2923b3746a6e2a046f8e330d
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-04-29 17:06:16 +0530

    Feat : Added Sidebar

---
+++ src/utils/Constants.jsx
@@ -0,0 +1,17 @@
+export const sidebarItems = [
+  {
+    path: "/",
+    icon: `explore`,
+    label: "Explore",
+  },
+  {
+    path: "/watchlist",
+    icon: `watch_later`,
+    label: "Watchlist",
+  },
+  {
+    path: "/history",
+    icon: `history`,
+    label: "History",
+  },
+];
