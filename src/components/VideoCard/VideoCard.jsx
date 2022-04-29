f173777be1168f27462b1a2901bcb0a90e289c32
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-04-29 17:11:07 +0530

    Feat : Added Card Variants

---
+++ src/components/VideoCard/VideoCard.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { useUserDetails } from "../../context/User/UserContext";
+import {
+  addToWatchlater,
+  removeFromWatchlater,
+} from "../../context/Video/Watchlater";
+import styles from "./VideoCard.module.css";
+
+export default function VideoCard({ video }) {
+  const { thumbnail, preview, title, creator, views } = video;
+  const [options, setOptions] = useState(false);
+  const { userState, userDispatch } = useUserDetails();
+  const { watchlater } = userState;
+
+  const optionsHandler = () => {
+    setOptions((prev) => !prev);
+  };
+
+  const watchlaterHandler = (video) => {
+    return watchlater?.some((item) => video._id === item._id);
+  };
+  return (
+    <div className={`${styles.video_card} box-shadow position-relative`}>
+      <div onClick={optionsHandler}>
+        <span
+          className={`position-absolute ${
+            options ? styles.hideOption : styles.showOption
+          } ${styles.options} material-icons`}
+        >
+          {options ? `close` : `more_vert`}
+        </span>
+      </div>
+
+      <div
+        className={`${styles.dialog_container} box-shadow ${
+          options ? styles.showDialog : styles.hideDialog
+        }`}
+      >
+        <div
+          onClick={() =>
+            watchlaterHandler(video)
+              ? removeFromWatchlater(video, userDispatch)
+              : addToWatchlater(video, userDispatch)
+          }
+          className={`${styles.dialog} flex-row-nowrap`}
+        >
+          <span className="material-icons">watch_later</span>
+          <p>
+            {watchlaterHandler(video)
+              ? "Del from Watch Later"
+              : "Add to Watch Later"}
+          </p>
+        </div>
+        <div className={`${styles.dialog} flex-row-nowrap`}>
+          <span className="material-icons">playlist_add</span>
+          <label htmlFor="watchlater">Add to Playlist</label>
+          <input id="watchlater" type="checkbox" />
+        </div>
+      </div>
+
+      <div className={styles.videoCover}>
+        <img
+          src={thumbnail}
+          className={styles.thumbnail}
+          alt="video-thumbnail"
+        />
+
+        <img src={preview} className={styles.preview} alt="video-thumbnail" />
+      </div>
+      <div className={styles.videoText}>
+        <h4 className={`${styles.title} body-2`}>{title}</h4>
+        <div className="flex-row-wrap flex-mid-left">
+          <p className="subtitle-2">{creator}</p>
+          <span className={` ${styles.verified} material-icons`}>verified</span>
+        </div>
+
+        <p>{views} Views</p>
+      </div>
+    </div>
+  );
+}
--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Explore.module.css";
 import { videos } from "../../backend/db/videos";
-
-//Will call the DB with API call in the Next Branch along with other API calls
+import { VideoCard } from "../../components";
+import { useVideo } from "../../context/Video/VideoContext";
 
 export default function Explore() {
+  const { videoState } = useVideo();
+  const { videolist } = videoState;
   return (
     <div className="flex-column-wrap flex-mid-center content">
       <div className={` ${styles.pill_container} default flex-row-wrap gap20`}>
@@ -14,32 +16,8 @@ export default function Explore() {
         <div className={`flex-row-wrap ${styles.pill}`}>Informative</div>
       </div>
       <div className={`${styles.videos_container} `}>
-        {videos.map((video) => (
-          <div className={`${styles.video_card} box-shadow`}>
-            <div className={styles.videoCover}>
-              <img
-                src={video.thumbnail}
-                className={styles.thumbnail}
-                alt="video-thumbnail"
-              />
-              <img
-                src={video.preview}
-                className={styles.preview}
-                alt="video-thumbnail"
-              />
-            </div>
-            <div className={styles.videoText}>
-              <h4 className={`${styles.title} body-2`}>{video.title}</h4>
-              <div className="flex-row-wrap flex-mid-left">
-                <p className="subtitle-2">{video.creator}</p>
-                <span className={` ${styles.verified} material-icons`}>
-                  verified
-                </span>
-              </div>
-
-              <p>{video.views} Views</p>
-            </div>
-          </div>
+        {videolist?.map((video) => (
+          <VideoCard key={video._id} video={video} />
         ))}
       </div>
     </div>
