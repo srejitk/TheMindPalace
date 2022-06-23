19ab89f0586f85ea9f1d74b0d2c850d888223ed9
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-06-24 03:40:59 +0530

    Style : Restyled Filter By and Sort By

--- src/components/CategoryChips/CategoryChips.jsx
+++ src/components/CategoryChips/CategoryChips.jsx
@@ -10,55 +10,69 @@ export default function CategoryChips() {
   const { categories } = useFilter();
 
   return (
-    <div className={` ${styles.pill_container} default flex-row-wrap gap20`}>
-      <button
-        className={`flex-row-wrap ${styles.pill} ${
-          category === "All" ? styles.active : ""
-        }`}
-        onClick={() => videoDispatch({ type: "SET_CATEGORY", payload: "All" })}
-      >
-        All
-      </button>
-      {categories?.map((category) => (
-        <button
-          key={category._id}
-          className={`flex-row-wrap ${styles.pill} ${
-            videoState?.category === category.categoryName ? styles.active : ""
-          }`}
-          onClick={() =>
-            videoDispatch({
-              type: "SET_CATEGORY",
-              payload: category.categoryName,
-            })
-          }
-        >
-          {category.categoryName}
-        </button>
-      ))}
-      <button
-        className={`flex-row-wrap ${styles.pill} ${
-          sortBy === "Latest" ? styles.active : ""
-        }`}
-        onClick={() =>
-          sortBy === "Latest"
-            ? videoDispatch({ type: "SET_SORT", payload: "" })
-            : videoDispatch({ type: "SET_SORT", payload: "Latest" })
-        }
-      >
-        Latest
-      </button>
-      <button
-        className={`flex-row-wrap ${styles.pill} ${
-          sortBy === "Oldest" ? styles.active : ""
-        }`}
-        onClick={() =>
-          sortBy === "Oldest"
-            ? videoDispatch({ type: "SET_SORT", payload: "" })
-            : videoDispatch({ type: "SET_SORT", payload: "Oldest" })
-        }
-      >
-        Oldest
-      </button>
+    <div className={` ${styles.pill_container} flex-row-wrap gap20`}>
+      <div className={styles.filters}>
+        <p className={`subtitle-2 ${styles.label}`}>FILTER BY</p>
+        <div className={styles.categoryContainer}>
+          <button
+            className={`flex-row-wrap subtitle-1 ${styles.pill} ${
+              category === "All" ? styles.active : ""
+            }`}
+            onClick={() =>
+              videoDispatch({ type: "SET_CATEGORY", payload: "All" })
+            }
+          >
+            All
+          </button>
+          {categories?.map((category) => (
+            <button
+              key={category._id}
+              className={`flex-row-wrap subtitle-1 ${styles.pill} ${
+                videoState?.category === category.categoryName
+                  ? styles.active
+                  : ""
+              }`}
+              onClick={() =>
+                videoDispatch({
+                  type: "SET_CATEGORY",
+                  payload: category.categoryName,
+                })
+              }
+            >
+              {category.categoryName}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className={`${styles.filters} ${styles.sort_filter}`}>
+        <p className={`subtitle-2 ${styles.label}`}>SORT BY </p>
+        <div className={styles.categoryContainer}>
+          <button
+            className={`flex-row-wrap subtitle-1 ${styles.pill} ${
+              sortBy === "Latest" ? styles.active : ""
+            }`}
+            onClick={() =>
+              sortBy === "Latest"
+                ? videoDispatch({ type: "SET_SORT", payload: "" })
+                : videoDispatch({ type: "SET_SORT", payload: "Latest" })
+            }
+          >
+            Latest
+          </button>
+          <button
+            className={`flex-row-wrap subtitle-1 ${styles.pill} ${
+              sortBy === "Oldest" ? styles.active : ""
+            }`}
+            onClick={() =>
+              sortBy === "Oldest"
+                ? videoDispatch({ type: "SET_SORT", payload: "" })
+                : videoDispatch({ type: "SET_SORT", payload: "Oldest" })
+            }
+          >
+            Oldest
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
